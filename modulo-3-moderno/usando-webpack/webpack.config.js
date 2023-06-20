7ede9c627bb7e9dc1c49e8a2c103c23dff9f471f
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-06-20 17:53:21 -0300

    loaders e plugins

---
+++ modulo-3-moderno/usando-webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }]
+    },
+    plugins:[
+        new MiniCssExtractPlugin()
+    ]
+  
+}
+
+//utilizando loaders
+//utilizando plugins
