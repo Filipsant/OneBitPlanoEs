0e301555ddadb7899eb90d1cfa6785bef84b1989
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-08-30 16:04:42 -0300

    começando React

---
+++ Modulo-8-React/Componetes React/index.jsx
@@ -0,0 +1,16 @@
+
+function Button(){
+    return <button>Meu Botão</button>
+}
+
+
+function Container() {
+    return(
+     <div className="">
+        <h1></h1>
+        <p></p>
+     </div>
+    )
+        
+    
+}
---
+++ Modulo-8-React/Conhecendo o JSX/index.jsx
@@ -0,0 +1,13 @@
+const element = <h1>Olá, Mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <h1>
+            texto
+        </h1>
+        <ul>
+            <li></li>
+            
+        </ul>
+    </div>
+)
