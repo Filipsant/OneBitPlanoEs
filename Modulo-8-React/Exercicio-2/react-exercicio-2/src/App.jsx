a0e218697cc68d516b2293a40d9f0bc01b8cde96
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-09-11 15:20:49 -0300

    props

--- Modulo-8-React/Exercicio-2/react-exercicio-2/src/App.jsx
+++ Modulo-8-React/Exercicio-2/react-exercicio-2/src/App.jsx
@@ -1,11 +1,13 @@
-import { Fragment } from 'react'
+
 import Card from './components/Card'
 
 function App() {
 return(
    <> 
     <h1>Exercicio 2</h1>
-       <Card />
+       <Card title="Poster: star wars (1977)" />
+       <Card title="Poster: empire strikes back (1980)" />
+       <Card title="Poster: return of the jadi(1983)" />
    </>
    
   
