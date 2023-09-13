9367871e69a25ab91154eca3a896748aac411d7a
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-09-13 15:43:57 -0300

    Renderizando condicionais

--- Modulo-8-React/Exercicio-4/react-exercicio-4/src/App.jsx
+++ Modulo-8-React/Exercicio-4/react-exercicio-4/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   const[password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
   const [passwordSize, setPasswordSize] = useState(12)
+  const [showInput, setShowInput]=useState(false)
+
+  const PasswordSizen = showInput ? setPassword : 8
 
   
 
@@ -34,10 +37,22 @@ function App() {
     <div className="app">
       <h1>Gerador de senhas</h1>
       <div>
+        <label htmlFor="showInput">Customizar tamanho:</label>
+        <input 
+        type="checkbox"
+         id="showInput" 
+         value={showInput}
+         onChange={()=> setShowInput(currentState => !currentState)}
+         />
+      </div>
+      {showInput ? (
+        <div>
         <label htmlFor="passwordSize">Tamanho:</label>
        <Input passwordSize = {passwordSize} setPasswordSize = {setPasswordSize}/>
       </div>
-      <button onClick={generate}>Gerar</button>
+      ): null}
+      
+      <button onClick={generate}>Gerar {PasswordSizen}</button>
       <button onClick={copyToClickboard}>{copyText}</button>
       <div>{password}</div>
     </div>
