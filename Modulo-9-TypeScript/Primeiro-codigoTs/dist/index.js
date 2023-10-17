8eb031695b82fd9dcabe2634f18d6f759a9dd7a9
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-10-17 15:48:56 -0300

    primeiro codigo em TypeScript

---
+++ Modulo-9-TypeScript/Primeiro-codigoTs/dist/index.js
@@ -0,0 +1,27 @@
+function sendSpaceship(name, capitain) {
+    const spaceship = {
+        name,
+        capitain,
+        speed: 20,
+        inMission: true,
+        crew: []
+    };
+    alert(`A nave ${spaceship.name}comandada pelo capitão${spaceship.capitain} foi enviada em uma missão`);
+    return spaceship;
+}
+function accelerate(targetSpeed, spaceship) {
+    if (spaceship.speed > targetSpeed) {
+        alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert(`Aumentando a velocidade da ${spaceship.name} para ${targetSpeed}...`);
+    }
+    else {
+        alert(`Mantendo a velocidade da ${spaceship.name}`);
+    }
+}
+const spaceshipName = prompt('insira o nome da nave a ser enviada ');
+const spaceshipCapitain = prompt('insira o nome do capitão ');
+const currentShip = sendSpaceship(spaceshipName, spaceshipCapitain);
+const speed = Number(prompt('insira a velocidade para qual dejesa acelerar. '));
+accelerate(speed, currentShip);
