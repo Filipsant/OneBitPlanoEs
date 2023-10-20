db2b6a9081f61553892569bdd7e3a520376cbfd3
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-10-20 15:30:05 -0300

    terminando exercicio

--- Modulo-9-TypeScript/Exercicio-1/src/index.ts
+++ Modulo-9-TypeScript/Exercicio-1/src/index.ts
@@ -18,7 +18,7 @@ function findSpaceship(name: string,){
     let spaceship:{
         name: string,
         pilot: string,
-        crewLimit: string,
+        crewLimit: number,
         crew: string[],
         inMission: boolean
     }
@@ -61,4 +61,99 @@ function firstMenuOption(){
         if(confirmation){
             addSpaceship(name,pilot, crewLimit)
         }
+}
+
+function secondMenuOption(){
+    const member  = prompt('Qual é o nome do tripulante?')
+    const spaceshipName = prompt(`Para qual nave ${member} deverá ser designado?`)
+
+    const spaceship = findSpaceship(spaceshipName)
+
+    if(spaceship){
+        const confirmation = confirm(`Confirma a inclusão de ${member} na tripulação da ${spaceship.name}?`)
+
+        if(confirmation){
+            addCrewMember(member, spaceship)
+        }
+    }
+
+    
+}
+
+function thirdMenuOption(){
+    const spaceshipName = prompt('Qual é o nome da nave a ser enviada?')
+
+    const spaceship = findSpaceship(spaceshipName)
+
+    if(spaceship){
+        const confirmation = confirm(`Confirme o envio da ${spaceship.name} na missão!`)
+
+        if(confirmation){
+            sendInMission(spaceship)
+        }
+    }
+}
+
+function fourthMenuOption(){
+    let list = 'Naves Registradas:\n'
+
+    spaceships.forEach((spaceship:{
+        name: string,
+        pilot: string,
+        crewLimit: number,
+        crew: string[],
+        inMission: boolean
+    }) => {
+        list +=`
+        Nave: ${spaceship.name}
+        Piloto: ${spaceship.pilot}
+        Em missão? ${spaceship.inMission ? 'sim' : 'não'}
+        Tamanho Máximo da Tripulação ${spaceship.crewLimit}
+        Tripulantes: ${spaceship.crew.length}  `
+
+        spaceship.crew.forEach(member =>{
+            list+= `   - ${member}\n   `
+        })
+
+
+    })
+
+    alert(list)
+}
+
+//Menu//
+
+let userOption = 0
+
+while (userOption !==5) {
+    const menu = `Painel Principal
+    1- Registar uma nova nave
+    2- Adicionar membro da tripulação
+    3- Enviar nave em missão
+    4-Listar naves resgistradas
+    5-Encerrar
+    `
+
+    userOption = Number.parseInt(prompt(menu))
+
+    switch (userOption){
+        case 1:
+            firstMenuOption()
+            break
+        case 2 :
+            secondMenuOption()
+            break
+        case 3 :
+            thirdMenuOption()
+            break
+        case 4 :
+            fourthMenuOption() 
+            break
+        case 5:
+            alert('Encerrando o sistema...')
+            break
+        default:
+            alert('Opção invalida! Retornar ao painel principal...')
+            break
+    }
 }
