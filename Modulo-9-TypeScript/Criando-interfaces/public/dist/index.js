18d1be6d904628e024cb02720283756172ab88b0
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-10-31 15:40:41 -0300

    criando interface

---
+++ Modulo-9-TypeScript/Criando-interfaces/public/dist/index.js
@@ -0,0 +1,18 @@
+let sun;
+sun.name = "Sol";
+sun.mass = 1.989 * (10 ** 30);
+sun.age = 4.683 * (10 ** 9);
+sun.planets = [];
+class MilkWayPlanet {
+    name;
+    mass;
+    population;
+    constructor(name, mass, population) {
+        this.name = name;
+        this.mass = mass;
+        this.population = population;
+    }
+    createSatellite(name) {
+        //...
+    }
+}
