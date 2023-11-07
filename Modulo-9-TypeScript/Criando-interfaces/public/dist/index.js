32a9010b30ff70c094c74cd7ed9243d78148e1e9
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-11-07 19:53:50 -0300

    exercicio de interface

--- Modulo-9-TypeScript/Criando-interfaces/public/dist/index.js
+++ Modulo-9-TypeScript/Criando-interfaces/public/dist/index.js
@@ -1,18 +1,72 @@
-let sun;
-sun.name = "Sol";
-sun.mass = 1.989 * (10 ** 30);
-sun.age = 4.683 * (10 ** 9);
-sun.planets = [];
-class MilkWayPlanet {
-    name;
-    mass;
-    population;
-    constructor(name, mass, population) {
-        this.name = name;
-        this.mass = mass;
-        this.population = population;
+const users = [];
+async function fetchUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const user = await response.json();
+    if (user.massage) {
+        alert('Usuario não encontrado');
     }
-    createSatellite(name) {
-        //...
+    else {
+        users.push(user);
+        alert(`O usuario ${user.login} foi salvo.\n` +
+            `\nid: ${user.id}` +
+            `\nlogin: ${user.login}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositorios públicos: ${user.public_repos}`);
     }
 }
+async function showUser(username) {
+    const user = users.find(user => user.login === username);
+    if (typeof user === 'undefined') {
+        alert('Usuario não encontrado!');
+    }
+    else {
+        const response = await fetch(user.repos_url);
+        const repos = await response.json();
+        let message = `id: ${user.id}\n` +
+            `\nlogin: ${user.login}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositorios Publicos: ${user.public_repos}`;
+        repos.forEach(repo => {
+            message += `\nNome:${repo.name}` +
+                `\nDescrição ${repo.description}` +
+                `\nEstrelas: ${repo.stargazers_count}` +
+                `\nÉ um fork: ${repo.fork ? 'Sim' : 'Não'}\n`;
+        });
+        alert(message);
+    }
+}
+function showAllUsers() {
+    let message = 'Usuarios:\n';
+    users.forEach(user => {
+        message += `\n- ${user.login}`;
+    });
+    alert(message);
+}
+function showReposTotal() {
+    const reposTotal = users.reduce((accumulator, user) => (accumulator + user.public_repos), 0);
+    alert(`O grupo possui um total de ${reposTotal} repositorios públicados!`);
+}
+function showTopFive() {
+    const topFive = users.slice().sort((a, b) => b.public_repos - a.public_repos).slice(0, 5);
+    let message = 'Top 5 usúarios com mais repositorios públicados:\n';
+    topFive.forEach((user, index) => {
+        message += `\n${index + 1}- ${user.login}: ${user.public_repos} repositórios`;
+    });
+    alert(message);
+}
+async function main() {
+    await fetchUser('isaacpontes');
+    await fetchUser('isaacpontes');
+    await fetchUser('julianaconde');
+    await fetchUser('pcaldass');
+    await fetchUser('lucasqueirogaa');
+    await fetchUser('frans203');
+    await fetchUser('LeDragoX');
+    await fetchUser('filipsant');
+    await showUser('filipsant');
+    await showUser('isaacpontes');
+    showAllUsers();
+    showReposTotal();
+    showTopFive();
+}
+main();
--- Modulo-9-TypeScript/Criando-interfaces/src/index.ts
+++ Modulo-9-TypeScript/Criando-interfaces/src/index.ts
@@ -1,43 +1,113 @@
-
-interface CelestialBody{
+interface GithubUserResponse{
+    id: number
+    login: string
     name: string
-    mass: number
+    bio: string
+    public_repos: number
+    repos_url: string
+    massage?: "Not Found"
 }
 
-interface Star extends CelestialBody{
-    age: number
-    planets: Planet[]
-}
-
-interface Planet extends CelestialBody {
-    population: number
-    createSatellite: (name:string)=> void
-}
-
-let sun: Star 
-
-sun.name = "Sol"
-sun.mass = 1.989 * (10 ** 30)
-sun.age = 4.683 * (10 ** 9)
-sun.planets = []
-
-class MilkWayPlanet implements Planet {
+interface GithubRepoResponse{
     name: string
-    mass: number
-    population: number
-    
+    description: string
+    fork: boolean
+    stargazers_count: number
 
-    constructor(name: string, mass: number, population: number){
-        this.name = name
-        this.mass = mass
-        this.population = population
-
-
-    }
-    
-    createSatellite(name: string){
-       //...
-    }
-
-  
 }
+
+const users: GithubUserResponse[]= []
+
+async function fetchUser(username:string) {
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    const user : GithubUserResponse = await response.json()
+
+    if(user.massage){
+        alert('Usuario não encontrado')
+    }else{
+        users.push(user)
+
+        alert(
+          `O usuario ${user.login} foi salvo.\n`+
+          `\nid: ${user.id}` +
+          `\nlogin: ${user.login}` +
+          `\nBio: ${user.bio}` +
+          `\nRepositorios públicos: ${user.public_repos}`
+        )
+    }
+}
+
+async function showUser (username:string) {
+    const user = users.find(user => user.login === username)
+
+    if(typeof user === 'undefined'){
+        alert('Usuario não encontrado!')
+    }else{
+        const response = await fetch(user.repos_url)
+        const repos: GithubRepoResponse[]= await response.json()
+
+        let message = 
+        `id: ${user.id}\n`+
+        `\nlogin: ${user.login}`+
+        `\nBio: ${user.bio}` +
+        `\nRepositorios Publicos: ${user.public_repos}` 
+    
+        repos.forEach(repo =>{
+            message += `\nNome:${repo.name}`+
+            `\nDescrição ${repo.description}`+
+            `\nEstrelas: ${repo.stargazers_count}`+
+            `\nÉ um fork: ${repo.fork? 'Sim': 'Não'}\n`
+    
+        })
+
+        alert(message)
+    }
+}
+
+function showAllUsers(){
+    let message = 'Usuarios:\n'
+
+    users.forEach(user =>{
+        message+= `\n- ${user.login}`
+    })
+    alert(message)
+}
+
+
+function showReposTotal(){
+    const reposTotal = users.reduce((accumulator, user)=>(accumulator + user.public_repos), 0)
+
+    alert(`O grupo possui um total de ${reposTotal} repositorios públicados!`)
+}
+
+function showTopFive(){
+    const topFive = users.slice().sort((a,b)=> b.public_repos - a.public_repos).slice(0,5)
+
+    let message = 'Top 5 usúarios com mais repositorios públicados:\n'
+
+    topFive.forEach((user, index)=>{
+        message+= `\n${index + 1}- ${user.login}: ${user.public_repos} repositórios`
+
+    })
+    alert(message)
+}
+
+async function main() {
+    await fetchUser('isaacpontes')
+    await fetchUser('isaacpontes')
+    await fetchUser('julianaconde')
+    await fetchUser('pcaldass')
+    await fetchUser('lucasqueirogaa')
+    await fetchUser('frans203')
+    await fetchUser('LeDragoX')
+    await fetchUser('filipsant')
+
+    await showUser('filipsant')
+    await showUser('isaacpontes')
+
+    showAllUsers()
+    showReposTotal()
+    showTopFive()
+}
+
+main()
